8a0a0d80f4443e9a5d7b49c993e36bbc2967172f
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-10-02 20:02:52 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_user
   has_many :users, through: :room_user
+  has_many :messages
+  
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
   has_many :room_user
   has_many :rooms, through: :room_user
+  has_many :messages
+  
+  validates :name, presence: true
 end
